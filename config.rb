6723393c5aafbd1a841f33f05faa1f402c6e21dc
Brian Sewell <bwsewell@gmail.com>
2014-01-21 15:50:20 -0500

    Setting up landing page for SCRC14

--- config.rb
+++ config.rb
@@ -3,9 +3,10 @@
 ###
 
 # Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+compass_config do |config|
+  config.output_style = :compact
+  config.line_comments = false
+end
 
 ###
 # Page options, layouts, aliases and proxies
@@ -45,19 +46,19 @@
 #   end
 # end
 
-set :css_dir, 'stylesheets'
+set :css_dir, 'assets/css'
 
-set :js_dir, 'javascripts'
+set :js_dir, 'assets/js'
 
-set :images_dir, 'images'
+set :images_dir, 'assets/img'
 
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
